242281b226a6fb02abc1fd827fb5cc64230ceca4
Ser4eg <105542161+Ser4eg@users.noreply.github.com>
2025-06-22 15:39:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homeworks from './homeworks/Homeworks';
 import Homework2 from './homeworks/Homework2/Homework2';
 import Homework3 from './homeworks/Homework3/Homework3';
+import Homework4 from './homeworks/Homework4/Homework4';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Homeworks />} />
         <Route path="/homework2" element={<Homework2 />} />
         <Route path="/homework3" element={<Homework3 />} />
+        <Route path="/homework4" element={<Homework4 />} />
       </Routes>
     </BrowserRouter>
   );
